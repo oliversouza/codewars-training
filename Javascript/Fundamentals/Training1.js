cdd335a81be48e27a62a8a2d515e6678fcd40a73
oliversouza <oliversouzadesigner@gmail.com>
2025-02-11 23:34:34 -0300

    Complete - You Can't Code Under Pressure #1

---
+++ Javascript/Fundamentals/Training1.js
@@ -0,0 +1,9 @@
+//You Can't Code Under Pressure #1
+//Code as fast as you can! You need to double the integer and return it.
+
+function doubleInteger(i) {
+    i *= 2;
+    return i;
+  }
+
+ console.log(doubleInteger (4));
