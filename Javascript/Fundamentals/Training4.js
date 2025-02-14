a2437e12ddd834cf5c0eba31d0df19ae77f39d89
oliversouza <oliversouzadesigner@gmail.com>
2025-02-13 23:51:47 -0300

    news trainings in Fundamentals

---
+++ Javascript/Fundamentals/Training4.js
@@ -0,0 +1,9 @@
+/*Create a function that accepts a parameter representing a name 
+and returns the message: "Hello, <name> how are you doing today?".
+
+[Make sure you type the exact thing I wrote or the program may not execute properly]
+*/
+
+function greet(name){
+    return `Hello, ${name} how are you doing today?`
+  }
